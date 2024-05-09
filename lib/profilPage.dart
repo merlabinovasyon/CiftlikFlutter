be1eb7ddcccea9ace91ffcad47a280ce88e1f149
merlab-inovasyon <f211213069@ktun.edu.tr>
2024-05-09 15:47:07 +0300

    Merge pull request #5 from Main

--- lib/profilPage.dart
+++ lib/profilPage.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:merlabciftlikyonetim/SyncService.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
 import 'package:sqflite/sqflite.dart';
 
